d7d373b166ac5fe146c7f1795ea5c1ffcdc02cf8
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-03-01 10:31:10 +0900

    ✨ LPの作成

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,13 +1,17 @@
+import { CallToAction } from "@/components/call-to-action";
+import { Features } from "@/components/features";
+import { Footer } from "@/components/footer";
+import { Hero } from "@/components/hero";
 import { MarketingHeader } from "@/components/marketing-header";
 
 const MarketingPage = async () => {
     return (
       <main className="min-h-screen">
         <MarketingHeader />
-        {/* <Hero /> */}
-        {/* <Features /> */}
-        {/* <CallToAction /> */}
-        {/* <Footer /> */}
+        <Hero />
+        <Features />
+        <CallToAction />
+        <Footer />
       </main>
     );
   };
